b3b392a15ab1ddae11824e4f003ad0900546f582
Tiago-Ribeiro <99698142+tiagoribeiroG@users.noreply.github.com>
2023-01-18 11:57:24 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+    fetch('https://sevencoders-starwars-wiki.herokuapp.com/films')
+    .then((data) => {
+        console.log({ data });
+    })
+
+    .catch((error) => {
+        console.log({ error });
+        alert('Erro ao carregar os Filmes');
+    })
+};
